55843b26ee7fb3f227fb1a7caed1a04d66b5d731
Jesus Lopez Vargas <vargas.lopez.jesus@gmail.com>
2018-12-03 17:31:11 +0100

    Success: Export to JSON

--- src/main/java/com/developer/AccessToJson.java
+++ src/main/java/com/developer/AccessToJson.java
@@ -1,5 +1,12 @@
 package com.developer;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -11,10 +18,15 @@ public class AccessToJson {
 	private Statement st;
 	private ResultSet r;
 	
+	BufferedWriter writeToJSON;
+
 	private boolean verbose = false;
 	
 	ArrayList<String> title;
 	ArrayList<Integer> column;
+	
+	private int[] groupColum;
+	private String groupName = "";
 	
 	private AccessToJson() {
 		title = new ArrayList<>();
@@ -34,39 +46,49 @@ public class AccessToJson {
 	}
 	
 	public void setVerbose(boolean verbose) {
+		getMessage("Set verbose mode ON.");
 		this.verbose = verbose;
 	}
 	
-	public void setPath(String PathAccessDB) {
+	public void setPath(String PathAccessDB, String NameTableDB) {
+		/**
+		 * Set the Path of the Database and the Table name to open it.
+		 */
 		try {
+			if (verbose)
+				getMessage("Opening the DB " + NameTableDB);
 			con = DriverManager.getConnection("jdbc:ucanaccess://" + PathAccessDB);
-			st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE );
-			getMessage("Success: Connection with Database");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Something goes wrong!");
-			getMessage("Failure: check Path of Database");
-		}
-	}
-	
-	public void setTable(String NameTableDB) {
-		try {
+			st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			if (verbose)
+				getMessage("Success: Connection with Database.");
 			r = st.executeQuery("SELECT * FROM " + NameTableDB);
-			getMessage("Success: sql command: SELECT * FROM " + NameTableDB);
+			if (verbose)
+				getMessage("Success: sql command: SELECT * FROM " + NameTableDB);
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out.println("Something goes wrong!");
-			getMessage("Failure: check the Table name of Database");
+			System.out.println("Something goes wrong!.");
+			getMessage("Failure: check Path or Table name of Database.");
 		}
 	}
 	
 	public void setColumn(String title, int column) {
+		/**
+		 * Add to a ArrayList<> the different key and value.
+		 */
 		this.title.add(title);
-		setColumnNumberACCESS(column);
+		this.column.add(column);
 	}
 	
-	private void setColumnNumberACCESS(int column) {
-		this.column.add(column);
+	public void setGroup(String title, int[] column) {
+		/**
+		 * If you need a Key and many value inside. Like a subgroup
+		 * Key : Value,
+		 *       Value,
+		 *       Value (...)
+		 */
+		this.groupColum = column;
+		this.groupName = title;
 	}
 	
 	private void getMessage(String text) {
@@ -75,5 +97,65 @@ public class AccessToJson {
 	}
 	
 	public void parseToJson(String fileToExport) {
+		try {
+			File file_exist = new File(fileToExport);
+			if (file_exist.exists()) {
+				System.out.println("File exist already, please choose a different file");
+				return;
+			}
+			writeToJSON = new BufferedWriter(new FileWriter(fileToExport, true));
+			if (verbose)
+				getMessage("New file " + fileToExport + " created.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Something goes wrong!.");
+			getMessage("Failure: Close the file " + fileToExport + ", and start again.");
+		}
+	}
+	
+	public void setToJSON() {
+		JSONArray listJason = new JSONArray();
+		JSONObject objJason = new JSONObject();
+		
+		String value;
+		int count = 1;
+		
+		try {
+			r.first();
+			while (r.isLast() == false) {
+				for (int x = 0; x < title.size(); x++) {
+					value = r.getString(column.get(x));
+					if (r.wasNull()) {
+						value = "";
+					}
+					objJason.put(title.get(x),value);
+				}
+				// Only if a Group exist
+				if (!(groupName.isEmpty())) {
+					for (int x = 0; x < groupColum.length; x++) {
+						value = r.getString(groupColum[x]);
+						getMessage(groupName + " - " + value);
+						listJason.add(value);
+					}
+					objJason.put(groupName, listJason);
+				}
+				writeToJSON.append(objJason.toString());
+				writeToJSON.newLine();
+				objJason.clear();
+				listJason.clear();
+				r.next();
+				count++;
+			}
+			r.close();
+			if (verbose)
+				getMessage("Read " + count + " lines of the DB.");
+			writeToJSON.close();
+			if (verbose)
+				getMessage("Success: Parse into JSON.");
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
